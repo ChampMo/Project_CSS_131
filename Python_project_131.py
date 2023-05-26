a0db81230ff6a8642cba4bb58643d01d60ad3de1
Monthol <97272854+ChampMo@users.noreply.github.com>
2023-05-26 18:40:46 +0700

    Update Python_project_131.py

--- Python_project_131.py
+++ Python_project_131.py
@@ -7,7 +7,7 @@ import tkinter as tk
 from tkinter import ttk
 
 def main():
-    token = 'q1kMJN5qnrWglFhOZBjVITYOrfT1bkUGNSMtPjiPMim'
+    token = 'ZHh6hPmgznso52lDaCH68sIxPdHAnbqoneViSOsZAEe'
     messenger = Sendline(token)
     city = ["bangkok","rayong","chiang mai","chiang rai"]
     tags = ["@5773","@5774","@5775","@5776"]
